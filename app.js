044a5834f8d4ff546504f5e8221d61d13d339bda
Andres <andres.ottonello@gmail.com>
2021-03-24 18:54:25 -0300

    Vista detalle 24-3

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3004
 const path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './views/detalle-alquiler.html'));
 });
 
 app.listen(port, () => {
